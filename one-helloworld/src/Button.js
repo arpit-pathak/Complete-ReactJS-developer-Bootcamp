9ba644e9c102490121102b54180c9b34be6d8797
code-today <blogarpit21@gmail.com>
2023-01-24 23:01:43 +0530

    props and default value

---
+++ one-helloworld/src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+const Button = ({ title = "Nothing" }) => (
+  <div>
+    <button className="button">{title}</button>
+  </div>
+);
+
+export default Button;
